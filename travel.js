7a2c3cd6d07ca7414048b09182531b78e3ddc04a
Katie <iamkatieakaicy@gmail.com>
2026-07-07 09:55:46 +0200

    Delete travel.js

--- travel.js
+++
@@ -1,3 +0,0 @@
-/* Optional YouTube Data API key. The page works without it using Firestore metadata.
-Restrict any key to the JAE HQ website and YouTube Data API v3. */
-export const YOUTUBE_API_KEY = "";
